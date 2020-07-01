aa29a98b505cb0414dccf192f000c79087acd986
michaljmalinowski <michal.j.malinowski@gmail.com>
2020-07-01 17:31:46 +0200

    Chenage name. Add SpringApplicaion class

---
+++ src/main/java/io/malinowski/michal/give_thing/GiveThingApplication.java
@@ -0,0 +1,13 @@
+package io.malinowski.michal.give_thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveThingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/malinowski/michal/give_thing/GiveThingApplicationTests.java
@@ -0,0 +1,16 @@
+package io.malinowski.michal.give_thing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiveThingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
