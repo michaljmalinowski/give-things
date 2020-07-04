97426f73d833896527b8c595dc87032e52b628a7
michaljmalinowski <michal.j.malinowski@gmail.com>
2020-07-04 15:43:39 +0200

    homepage - get info about donation from database

--- src/main/java/io/malinowski/michal/give_thing/HomeController.java
+++ src/main/java/io/malinowski/michal/give_thing/HomeController.java
@@ -1,26 +1,27 @@
 package io.malinowski.michal.give_thing;
 
 import io.malinowski.michal.give_thing.domain.dao.InstitutionDao;
-import io.malinowski.michal.give_thing.domain.model.Institution;
+import io.malinowski.michal.give_thing.service.DonationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 
 @Controller
 public class HomeController {
     private final InstitutionDao institutionDao;
+    private final DonationService donationService;
 
-    public HomeController(InstitutionDao institutionDao) {
+    public HomeController(InstitutionDao institutionDao, DonationService donationService) {
         this.institutionDao = institutionDao;
+        this.donationService = donationService;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
-        List<Institution> institutions = institutionDao.findAll();
-        model.addAttribute("institutions", institutions);
+        model.addAttribute("institutions", institutionDao.findAll());
+        model.addAttribute("countDonation", donationService.countDonation());
+        model.addAttribute("countSack", donationService.countSack());
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -18,7 +18,7 @@
 <section class="stats">
     <div class="container container--85">
         <div class="stats--item">
-            <em>13</em>
+            <em>${countSack}</em>
 
             <h3>Oddanych worków</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius est beatae, quod accusamus illum
@@ -26,7 +26,7 @@
         </div>
 
         <div class="stats--item">
-            <em>5</em>
+            <em>${countDonation}</em>
             <h3>Przekazanych darów</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam magnam, sint nihil cupiditate quas
                 quam.</p>
